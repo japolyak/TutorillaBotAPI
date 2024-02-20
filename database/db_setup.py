14c798892a0a620bfa178ef19601f65690b322df
japolyak <artemkryvolap@gmail.com>
2024-02-20 07:56:14 +0100

    feat(start) Deployment test

--- database/db_setup.py
+++ database/db_setup.py
@@ -4,13 +4,15 @@ from sqlalchemy.orm import sessionmaker
 from config import db_username as username, db_password, db_host, db_port, db_name
 
 
-DATABASE_URL = f"postgresql+psycopg2://{username}:{db_password}@{db_host}:{db_port}/{db_name}"
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# DATABASE_URL = f"postgresql+psycopg2://{username}:{db_password}@{db_host}:{db_port}/{db_name}"
+# engine = create_engine(DATABASE_URL, echo=True)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False)
 
 
 def init_db():
-    Base.metadata.create_all(bind=engine)
+    print('test')
+    # Base.metadata.create_all(bind=engine)
 
 
 def get_db():
