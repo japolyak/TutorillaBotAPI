922dc735e8fb5a8ea853aef8841cffb40bd7ecbc
japolyak <artemkryvolap@gmail.com>
2024-03-22 00:35:06 +0100

    test3

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from config import telegram_web_app, my_tg_id
 # from database.db_setup import init_db
-from routes.admin_routes import router as admin_router
-from routes.private_course_routes import router as private_course_router
-from routes.subject_routes import router as subject_router
-from routes.tutor_course_routes import router as tutor_course_router
-from routes.user_routes import router as user_router
-from routes.web_app_routes import router as web_app_router
+# from routes.admin_routes import router as admin_router
+# from routes.private_course_routes import router as private_course_router
+# from routes.subject_routes import router as subject_router
+# from routes.tutor_course_routes import router as tutor_course_router
+# from routes.user_routes import router as user_router
+# from routes.web_app_routes import router as web_app_router
 from bot_client.message_sender import send_test_message
 
 app = FastAPI()
@@ -29,9 +29,9 @@ async def root():
     send_test_message(my_tg_id)
     return {"message": "Hello World!"}
 
-app.include_router(admin_router, prefix="/admin", tags=["admin"])
-app.include_router(private_course_router, prefix="/private-courses", tags=["private-courses"])
-app.include_router(subject_router, prefix="/subjects", tags=["subjects"])
-app.include_router(tutor_course_router, prefix="/tutor-courses", tags=["tutor-courses"])
-app.include_router(user_router, prefix="/users", tags=["users"])
-app.include_router(web_app_router, prefix="/auth", tags=["web_app_authorization"])
+# app.include_router(admin_router, prefix="/admin", tags=["admin"])
+# app.include_router(private_course_router, prefix="/private-courses", tags=["private-courses"])
+# app.include_router(subject_router, prefix="/subjects", tags=["subjects"])
+# app.include_router(tutor_course_router, prefix="/tutor-courses", tags=["tutor-courses"])
+# app.include_router(user_router, prefix="/users", tags=["users"])
+# app.include_router(web_app_router, prefix="/auth", tags=["web_app_authorization"])
