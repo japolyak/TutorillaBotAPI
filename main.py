376bfa90833112ce7d82cfb6af09f7fc577e6f43
japolyak <artemkryvolap@gmail.com>
2024-03-23 20:59:19 +0100

    test to use routes with db

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from config import telegram_web_app
 from database.db_setup import init_db
 from routes.admin_routes import router as admin_router
-# from routes.private_course_routes import router as private_course_router
+from routes.private_course_routes import router as private_course_router
 # from routes.subject_routes import router as subject_router
 # from routes.tutor_course_routes import router as tutor_course_router
 # from routes.user_routes import router as user_router
@@ -26,7 +26,7 @@ init_db()
 
 app.include_router(test_router, prefix="/test", tags=["test"])
 app.include_router(admin_router, prefix="/admin", tags=["admin"])
-# app.include_router(private_course_router, prefix="/private-courses", tags=["private-courses"])
+app.include_router(private_course_router, prefix="/private-courses", tags=["private-courses"])
 # app.include_router(subject_router, prefix="/subjects", tags=["subjects"])
 # app.include_router(tutor_course_router, prefix="/tutor-courses", tags=["tutor-courses"])
 # app.include_router(user_router, prefix="/users", tags=["users"])
