c6598f08e36927b0e74111702feb95e47c6b8a7d
japolyak <artemkryvolap@gmail.com>
2024-04-04 00:36:00 +0200

    Small change

--- routes/test_routes.py
+++ routes/test_routes.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
 from bot_client.message_sender import send_test_message
 from config import my_tg_id
 
 
 router = APIRouter()
 
+
 @router.post("/")
 async def root():
-    send_test_message(my_tg_id)
     return {"message": "Hello World!"}
