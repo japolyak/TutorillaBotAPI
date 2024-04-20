4382fa179b29126b8817629568b7f67e7c8298ab
japolyak <artemkryvolap@gmail.com>
2024-04-21 00:12:53 +0200

    feat: Subjects url simplified

--- routes/subject_routes.py
+++ routes/subject_routes.py
@@ -1,4 +1,4 @@
-from fastapi import status, APIRouter, Depends, HTTPException
+from fastapi import status, APIRouter, Depends
 from .data_transfer_models import SubjectDto, Role
 from sqlalchemy.orm import Session
 from database.db_setup import session
@@ -11,33 +11,16 @@ from typing import Literal
 router = APIRouter()
 
 
-@router.get(path="/available-subjects/users/{user_id}/", status_code=status.HTTP_200_OK,
-            response_model=list[SubjectDto], summary="Get available courses to teach or learn by user id")
-async def get_available_courses(user_id: int, role: Literal[Role.Student, Role.Tutor], db: Session = Depends(session)):
-    # TODO - rewrite
-    if role == Role.Student:
-        db_available_subjects = subject_crud.get_available_subjects_student(db=db, user_id=user_id)
-        subjects = jsonable_encoder(db_available_subjects)
+@router.get(path="/users/{user_id}/available/{is_available}/", status_code=status.HTTP_200_OK,
+            summary="Gets users subjects by user id", response_model=list[SubjectDto])
+async def get_subjects(user_id: int, is_available: bool, role: Literal[Role.Student, Role.Tutor], db: Session = Depends(session)):
+    role_to_function = {
+        Role.Student: subject_crud.get_student_subjects,
+        Role.Tutor: subject_crud.get_tutor_subjects
+    }
 
-        return JSONResponse(status_code=status.HTTP_200_OK, content=subjects)
+    subjects_function = role_to_function.get(role)
 
-    db_available_subjects = subject_crud.get_available_subjects_tutor(db=db, user_id=user_id)
-    subjects = jsonable_encoder(db_available_subjects)
-
-    return JSONResponse(status_code=status.HTTP_200_OK, content=subjects)
-
-
-@router.get(path="/users/{user_id}/", status_code=status.HTTP_200_OK, summary="Gets users subjects by user id",
-            response_model=list[SubjectDto])
-async def get_subjects(user_id: int, role: Literal[Role.Student, Role.Tutor], db: Session = Depends(session)):
-    # TODO - rewrite
-    if role == "student":
-        db_student_subjects = subject_crud.get_student_subjects(db=db, user_id=user_id)
-        subjects = jsonable_encoder(db_student_subjects)
-
-        return JSONResponse(status_code=status.HTTP_200_OK, content=subjects)
-
-    db_tutor_subjects = subject_crud.get_tutor_courses(db=db, user_id=user_id)
-    subjects = jsonable_encoder(db_tutor_subjects)
+    subjects = jsonable_encoder(subjects_function(db, user_id, is_available))
 
     return JSONResponse(status_code=status.HTTP_200_OK, content=subjects)
